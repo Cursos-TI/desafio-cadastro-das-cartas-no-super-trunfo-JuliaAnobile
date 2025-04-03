dce53e469018faeea8f535e5c3ee941d9d1baa9b
JuliaAnobile <juliaanobileads@gmail.com>
2025-04-03 22:20:35 +0000

    programa finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,39 +1,110 @@
 #include <stdio.h>
 
-int main() {
+int main () {
      \\carta1
-     char estado;
-     char código da carta [3];
-     char nome_cidade [20];
-     float populacao;
-     float area;
-     float PIB;
-     int pontos_turisticos;
- 
-     printf("estado (M-Z): ");
-     scanf("%c", &estado);
- 
-     printf("codigo: ");
-     scanf("%s", &codigo);
- 
-     printf("Nome da cidade: ");
-     scanf("%s", &nome_cidade);
- 
-     printf("população: ");
-     scanf("%d", &populacao);
- 
-     printf("area: ");
-     scanf("%f", &area);
-     
-     printf("PIB: ");
-     scanf("%f", &PIB);
- 
-     printf("Números de pontos turísticos: ");
-     scanf("%d", &pontos_turisticos);
- 
+  char estado;
+  char codigo da carta [3];
+  char nome_cidade [20];
+  float populacao;
+  float area;
+  float PIB;
+  int pontos_turisticos;
+
+  printf("estado (M-Z): ");
+  scanf("%c", &estado);
+
+  printf("codigo: ");
+  scanf("%s", &codigo);
+
+  printf("Nome da cidade: ");
+  scanf("%s", &nome_cidade);
+
+  printf("populacao: ");
+  scanf("%d", &populacao);
+
+  printf("area: ");
+  scanf("%f", &area);
   
+  printf("PIB: ");
+  scanf("%f", &PIB);
+
+  printf("Números de pontos turisticos: ");
+  scanf("%d", &pontos_turisticos);
+
+\\printf para imprimir os resultados
+
+printf("o estado é:%c\n" ,estado);
+
+printf("codigo é:%s\n" ,codigo);
+
+printf("nome da cidade é:%s\n" ,nome_cidade);
+
+printf("a populacao é:%d\n" ,populacao);
+
+printf("area é:%.2f metros km\n",area);
+
+printf("o PIB é:%f\n" ,pib);
+
+printf("numeros de pontos turisticos é:%d\n" ,pontos_turisticos);
+
+
+
+\\carta 2
+ int main () {
+     \\carta2
+  char estado;
+  char codigo da carta [4];
+  char nome_cidade [20];
+  float populacao;
+  float area;
+  float PIB;
+  int pontos_turisticos;
+
+  printf("estado (M-Z): ");
+  scanf("%c", &estado);
+
+  printf("codigo: ");
+  scanf("%s", &codigo);
+
+  printf("Nome da cidade: ");
+  scanf("%s", &nome_cidade);
+
+  printf("populacao: ");
+  scanf("%d", &populacao);
+
+  printf("area: ");
+  scanf("%f", &area);
   
-  
+  printf("PIB: ");
+  scanf("%f", &PIB);
+
+  printf("Números de pontos turisticos: ");
+  scanf("%d", &pontos_turisticos);
+
+\\printf para imprimir os resultados
+
+printf("o estado é:%c\n" ,estado);
+
+printf("codigo é:%s\n" ,codigo);
+
+printf("nome da cidade é:%s\n" ,nome_cidade);
+
+printf("a populacao é:%d\n" ,populacao);
+
+printf("area é:%.2f metros km\n",area);
+
+printf("o PIB é:%f\n" ,pib);
+
+printf("numeros de pontos turisticos é:%d\n" ,pontos_turisticos);
+
+
+
+
+
+  return 0;
+}
+
+
   
   
   
