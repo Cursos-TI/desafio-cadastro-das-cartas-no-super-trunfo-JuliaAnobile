a7ca410c3d3682b2a79f96c76e9d265a4ebb5fcf
JuliaAnobile <juliaanobileads@gmail.com>
2025-04-03 21:41:52 +0000

    COMEÇANDO DESAFIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+    //carta1
+    char estado;
+    char codigo[3];
+    char nome_cidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
